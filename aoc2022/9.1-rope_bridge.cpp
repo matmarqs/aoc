ca608ca75a05ac5b754a0a2b3a12f866e229c25d
Mateus Marques <mateusmarques2001@usp.br>
2022-12-10 10:47:04 -0300

    day 9

---
+++ aoc2022/9.1-rope_bridge.cpp
@@ -0,0 +1,116 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <set>
+using namespace std;
+
+/* DEFINITIONS */
+
+struct pos {
+    int x;
+    int y;
+    bool operator< (const pos &that) const {
+        if (this->x < that.x) return 1;
+        else if (this->x == that.x) return (this->y < that.y);
+        else return 0;
+    }
+};
+
+void move_head(char direction, pos *head) {
+    switch (direction) {
+        case 'R':
+            head->x++;
+            break;
+        case 'L':
+            head->x--;
+            break;
+        case 'U':
+            head->y++;
+            break;
+        case 'D':
+            head->y--;
+            break;
+        default:
+            break;
+    }
+}
+
+int touching(pos *tail, pos *head) {
+    return (abs(head->x - tail->x) <= 1) && (abs(head->y - tail->y) <= 1);
+}
+
+int diagonal(pos tail, pos *head, int dx, int dy) {
+    tail.x = tail.x + dx, tail.y = tail.y + dy;
+    return touching(&tail, head);
+}
+
+void move_tail(pos *tail, pos *head, char direction) {
+    if (head->y == tail->y) {   // same line
+        if (head->x - tail->x == 2)
+            tail->x++;
+        else if (head->x - tail->x == -2)
+            tail->x--;
+    }
+    else if (head->x == tail->x) {  // same column
+        if (head->y - tail->y == 2)
+            tail->y++;
+        else if (head->y - tail->y == -2)
+            tail->y--;
+    }
+    else if (!touching(tail, head)) {  // diagonal move
+        if (diagonal(*tail, head, 1, 1))
+            tail->x++, tail->y++;
+        else if (diagonal(*tail, head, -1, 1))
+            tail->x--, tail->y++;
+        else if (diagonal(*tail, head, -1, -1))
+            tail->x--, tail->y--;
+        else if (diagonal(*tail, head, 1, -1))
+            tail->x++, tail->y--;
+    }
+}
+
+void print_map(const pos *tail, const pos *head) {
+    for (int y = 4; y >= 0; y--) {
+        for (int x = 0; x < 7; x++) {
+            char c = 0;
+            if (x == 0 && y == 0)
+                c = 's';
+            if (x == tail->x && y == tail->y)
+                c = 'T';
+            if (x == head->x && y == head->y)
+                c = 'H';
+            if (!c)
+                c = '.';
+            putchar(c);
+        }
+        putchar('\n');
+    }
+    putchar('\n');
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <file>" << endl;
+        return EXIT_FAILURE;   // failure
+    }
+
+    ifstream InputFile(argv[1]);
+
+    /* read until end of file */
+    string line;
+    char direction; int n_steps;
+    pos head = {0, 0}, tail = {0, 0};
+    set <pos> visited;
+    while (getline(InputFile, line)) {
+        sscanf(line.c_str(), "%c %d", &direction, &n_steps);
+        for ( ; n_steps > 0; n_steps--) {
+            move_head(direction, &head);
+            move_tail(&tail, &head, direction);
+            visited.insert(tail);
+            //print_map(&tail, &head);
+        }
+    }
+    cout << visited.size() << endl;
+}
